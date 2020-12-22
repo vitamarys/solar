8ca156a32eebc754266af22c010eb2342a098fd8
vitamarys <vitaliy.marysiy@gmail.com>
2020-12-22 16:48:37 +0200

    add news-sp page

--- scripts/slider.js
+++ scripts/slider.js
@@ -4,8 +4,21 @@ $(document).ready(function(){
         slidesToScroll: 1,
         infinite: true,
         variableWidth:true,
-        appendArrows:$('.arrows')
+         appendArrows:$('.arrows')
     });
-  
-
+  $('.slider-sun-power-for').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    // infinite: false,
+    arrows:true,
+    asNavFor: '.slider-sun-power-nav'
+  });
+$('.slider-sun-power-nav').slick({
+    slidesToShow: 3,
+    asNavFor: '.slider-sun-power-for',
+    focusOnSelect: true,
+    slidesToScroll: 1,
+    easing:'ease-in',
+    autoplay: false,
+})
 })
